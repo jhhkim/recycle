abbbeb97d14b59815515b8c41a6859ddc5e4aadc
jhhkim <aekim7@naver.com>
2021-12-14 10:06:46 +0900

    로그인 추가

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,56 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+var info = document.getElementById("busan");
+
+function newinfo() {
+    if(info.value==1){
+        window.location.href="https://www.bsgangseo.go.kr/news/view.do?nIdx=8785&mId=0101000000";
+    }
+    if(info.value==2){
+        window.location.href="https://www.geumjeong.go.kr/board/view.geumj?boardId=BBS_0000004&menuCd=DOM_000001014004001000&dataSid=890882";
+    }
+    if(info.value==3){
+        window.location.href="https://www.gijang.go.kr/board/view.gijang?boardId=BBS_0000002&menuCd=DOM_000000101001001000&dataSid=189981";
+    }
+    if(info.value==4){
+        location.href="../recycleExchange/noresult.html";
+    }
+    if(info.value==5){
+        window.location.href="http://www.bsdonggu.go.kr/index.donggu?menuCd=DOM_000000104005008000";
+    }
+    if(info.value==6){
+        window.location.href="https://www.dongnae.go.kr/board/view.dongnae?boardId=BBS_0000135&menuCd=DOM_000000207006000000&dataSid=828389";
+    }
+    if(info.value==7){
+        window.location.href="https://www.busanjin.go.kr/board/view.busanjin?menuCd=DOM_000000110001001000&boardId=BBS_0000009&dataSid=3826420";
+    }
+    if(info.value==8){
+        location.href="../recycleExchange/noresult.html";
+    }
+    if(info.value==9){
+        window.location.href="https://www.sasang.go.kr/board/view.sasang?boardId=BBS_0000100&menuCd=DOM_000000903000000000&dataSid=478853";
+    }
+    if(info.value==10){
+        window.location.href="https://www.saha.go.kr/dadae1/bbs/view.do?mId=0501000000&bIdx=121255&ptIdx=539";
+    }
+    if(info.value==11){
+        window.location.href="https://www.bsseogu.go.kr/news/board/view.bsseogu?boardId=BBS_0000111&menuCd=DOM_000000507000000000&dataSid=168754";
+    }
+    if(info.value==12){
+        window.location.href="https://www.suyeong.go.kr/board/view.suyeong?boardId=BBS_0000085&menuCd=DOM_000002301000000000&dataSid=173218&cpath=news&keywordEtc1=289";
+    }
+    if(info.value==13){
+        window.location.href="https://www.yeonje.go.kr/dong/bbs/view.do?bIdx=98106&ptIdx=113&mId=0401000000";
+    }
+    if(info.value==14){
+        window.location.href="https://www.yeongdo.go.kr/00000/00007/00011.web?amode=view&gcode=1027&idx=304929";
+    }
+    if(info.value==15){
+        window.location.href="http://www.bsjunggu.go.kr/dong/board/view.junggu?boardId=BBS_0000112&menuCd=DOM_000001101004002000&dataSid=207612";
+    }
+    if(info.value==16){
+        window.location.href="https://www.haeundae.go.kr/board/view.do?boardId=BBS_0000038&menuCd=DOM_000000101011004002&orderBy=REGISTER_DATE%20DESC&paging=ok&startPage=34&dataSid=3069396";
+    }
+}
